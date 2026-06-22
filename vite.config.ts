865eb0fe600fca67859d077f97769160e7812f16
projectcode.dev <noreply@projectcode.dev>
2026-06-22 18:13:03 +0530

    chore: sync with ritikmaurya18/simple-react-ecommerce@master

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { host: '::', port: 9047 },
+  server: { host: '::', port: 8000 },
   plugins: [react()],
 })
